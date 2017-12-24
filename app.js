3460c843dbcd3658acc23e2bb274c1acdd3328c3
messiahhh <616348105@qq.com>
2017-12-25 01:09:36 +0800

    t

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const iconv = require('iconv-lite')
 const fs = Promise.promisifyAll(require('fs'))
+const Url = require('url')
+const qs = require('querystring')
 //your cookie
-let cookie = 'Hm_lvt_d72896ddbf8d27c750e3b365ea2fc902=1513864849,1513867150; Hm_lpvt_d72896ddbf8d27c750e3b365ea2fc902=1513867150; PHPSESSID=v5nl20bel8cs053aie22d13b05ir3khj; jieqiUserInfo=jieqiUserId%3D312317%2CjieqiUserName%3D2497360927%2CjieqiUserGroup%3D3%2CjieqiUserVip%3D0%2CjieqiUserName_un%3D2497360927%2CjieqiUserHonor_un%3D%26%23x65B0%3B%26%23x624B%3B%26%23x4E0A%3B%26%23x8DEF%3B%2CjieqiUserGroupName_un%3D%26%23x666E%3B%26%23x901A%3B%26%23x4F1A%3B%26%23x5458%3B%2CjieqiUserLogin%3D1513867166; jieqiVisitInfo=jieqiUserLogin%3D1513867166%2CjieqiUserId%3D312317'
+let cookie = 'Hm_lvt_acfbfe93830e0272a88e1cc73d4d6d0f=1513864851,1513867609,1513955689; Hm_lvt_d72896ddbf8d27c750e3b365ea2fc902=1513864849,1513867150,1513955666,1514047381; PHPSESSID=7ahmld2a0tj5m28v1bl9qgh9r9488hae; jieqiUserInfo=jieqiUserId%3D312317%2CjieqiUserName%3D2497360927%2CjieqiUserGroup%3D3%2CjieqiUserVip%3D0%2CjieqiUserName_un%3D2497360927%2CjieqiUserHonor_un%3D%26%23x65B0%3B%26%23x624B%3B%26%23x4E0A%3B%26%23x8DEF%3B%2CjieqiUserGroupName_un%3D%26%23x666E%3B%26%23x901A%3B%26%23x4F1A%3B%26%23x5458%3B%2CjieqiUserLogin%3D1514047377; jieqiVisitInfo=jieqiUserLogin%3D1514047377%2CjieqiUserId%3D312317; Hm_lpvt_d72896ddbf8d27c750e3b365ea2fc902=1514047392'
 
 class Novel {
     constructor(obj) {
@@ -40,24 +42,37 @@ class Novel {
 
     static async download (url) {
         let $ = await this.get(url)
+        let backUrl = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.'))
+        let id
+        if (backUrl === 'articleinfo') {
+            id = `1-${qs.parse(Url.parse(url).query).id}`
+        }
+        else {
+            id = `0-${backUrl}`
+        }
         let novel = new Novel({
-            id: url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.')),
+            id,
             name: $('b').eq(2).text(),
             desc: $('.hottext:nth-of-type(4)').nextAll('span').text(),
             indexUrl: url,
             catalogUrl: $('#content').children().first().children().eq(5).children().children().first().find('a').attr("href")
         })
         Novel.mkdirSingleNovel(novel.id)
-        let catalogBaseUrl = novel.catalogUrl.substring(0, novel.catalogUrl.lastIndexOf('/'))
-        $ = await this.get(novel.catalogUrl)
-        // interesting point:
-        // $('table td a').map() return a cheerio Object rather than, which can't be used in Promise.all. So we need to add a get() after .map()
-        await Promise.all($('table td a').map(async (index, item) => {
-            let url = `${catalogBaseUrl}/${$(item).attr('href')}`
-            let $$ = await this.get(url)
-            await fs.writeFileAsync(`./novels/${novel.id}/${index+1}.md`, $$('#content').text())
-            console.log(`${novel.name}[id=${novel.id}]第${index+1}章已下载完成`)
-        }).get())
+        //fuck wenku8, some pages's structure is so strange.
+        if (novel.catalogUrl !== undefined) {
+            let catalogBaseUrl = novel.catalogUrl.substring(0, novel.catalogUrl.lastIndexOf('/'))
+            $ = await this.get(novel.catalogUrl)
+            // interesting point:
+            // $('table td a').map() return a cheerio Object rather than, which can't be used in Promise.all. So we need to add a get() after .map()
+            await Promise.all($('table td a').map(async (index, item) => {
+                let url = `${catalogBaseUrl}/${$(item).attr('href')}`
+                let $$ = await this.get(url)
+                await fs.writeFileAsync(`./novels/${novel.id}/${index+1}.md`, $$('#content').text())
+                // if ($('table td a').length === index + 1) {
+                    console.log(`${novel.name}[id=${novel.id}]${index+1}章节已下载完成`)
+                // }
+            }).get())
+        }
     }
 }
 module.exports = Novel
--- test.js
+++ test.js
@@ -1,135 +1,32 @@
+const Promise = require('bluebird')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const iconv = require('iconv-lite')
-const fs = require('fs')
-const url = require('url')
-
-let index = 'http://www.wenku8.com/index.php'
-let indexHost = 'www.wenku8.com'
-
-let pageArr = []
-// async function getNovelsUrl() {
-//     let res = await axios.get(index, {
-        headers: {
-            cookie: 'jieqiUserInfo=jieqiUserId%3D312317%2CjieqiUserName%3D2497360927%2CjieqiUserGroup%3D3%2CjieqiUserVip%3D0%2CjieqiUserName_un%3D2497360927%2CjieqiUserHonor_un%3D%26%23x65B0%3B%26%23x624B%3B%26%23x4E0A%3B%26%23x8DEF%3B%2CjieqiUserGroupName_un%3D%26%23x666E%3B%26%23x901A%3B%26%23x4F1A%3B%26%23x5458%3B%2CjieqiUserLogin%3D1506504225; jieqiVisitInfo=jieqiUserLogin%3D1506504225%2CjieqiUserId%3D312317; Hm_lvt_acfbfe93830e0272a88e1cc73d4d6d0f=1506503189;    Hm_lpvt_acfbfe93830e0272a88e1cc73d4d6d0f=1506504664; PHPSESSID=fplbbhg8ls6cn280v40psqksi762sjad; Hm_lvt_d72896ddbf8d27c750e3b365ea2fc902=1506503189; Hm_lpvt_d72896ddbf8d27c750e3b365ea2fc902=1506520070'
-        },
-//         responseType: 'arraybuffer'
-//     })
-//     let $ = cheerio.load(iconv.decode(res.data, 'gbk'))
-//     $('#left .ultop a').each(async (index, item) => {
-//             let url = $(item).attr('href')
-//             let res = await axios.get(url, {
-//                 headers: {
-//                     responseType: 'arraybuffer'
-//                 }
-//             })
-//             let $ = cheerio.load(iconv.decode(res.data, 'gbk'))
-//             $('#centerm a').each((index, item) => {
-//                 let pageUrl = $(item).attr('href')
-//                 novelArr.add(pageUrl)
-//             })
-//     })
-// }
-//
-// getNovelsUrl()
-//
-
-
-
-
-
-
-
-
-
-
-
-axios.get(index, {
-    headers: {
-        cookie: 'jieqiUserInfo=jieqiUserId%3D312317%2CjieqiUserName%3D2497360927%2CjieqiUserGroup%3D3%2CjieqiUserVip%3D0%2CjieqiUserName_un%3D2497360927%2CjieqiUserHonor_un%3D%26%23x65B0%3B%26%23x624B%3B%26%23x4E0A%3B%26%23x8DEF%3B%2CjieqiUserGroupName_un%3D%26%23x666E%3B%26%23x901A%3B%26%23x4F1A%3B%26%23x5458%3B%2CjieqiUserLogin%3D1506504225; jieqiVisitInfo=jieqiUserLogin%3D1506504225%2CjieqiUserId%3D312317; Hm_lvt_acfbfe93830e0272a88e1cc73d4d6d0f=1506503189; Hm_lpvt_acfbfe93830e0272a88e1cc73d4d6d0f=1506504664; PHPSESSID=fplbbhg8ls6cn280v40psqksi762sjad; Hm_lvt_d72896ddbf8d27c750e3b365ea2fc902=1506503189; Hm_lpvt_d72896ddbf8d27c750e3b365ea2fc902=1506520070'
-    },
-    responseType: 'arraybuffer'
-})
-.then((res) => {
-    let $ = cheerio.load(iconv.decode(res.data, 'gbk'))
-    let pageArr = []
-    $('#left .ultop a').each((index, item) => {
-        let pageUrl = $(item).attr('href')
-        pageArr.push(pageUrl)
-    })
-
-    return new Promise((resolve, reject) => {
-        let novelArr = new Set()
-        let promiseArr = []
-        pageArr.forEach((item, index) => {
-            let promise = axios.get(item, {
-                responseType: 'arraybuffer'
-            })
-            .then((res) => {
-                let $ = cheerio.load(iconv.decode(res.data, 'gbk'))
-                $('#centerm a').each((index, item) => {
-                    let pageUrl = $(item).attr('href')
-                    novelArr.add(pageUrl)
-                })
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-            promiseArr.push(promise)
-        })
-        Promise.all(promiseArr).then((data) => {
-            resolve(novelArr)
-        }).catch((err) => {
-            console.log(err)
-        })
-    })
-})
-.then((data) => {
-    return new Promise((resolve, reject) => {
-        // console.log('1')
-        let promiseArr = []
-        let urls = new Set()
-        for (let i of data) {
-
-
-            if (url.parse(i).hostname === indexHost) {
-                let promise = axios.get(i, {
-                    responseType: 'arraybuffer'
-                }).then((res) => {
-                    let $ = cheerio.load(iconv.decode(res.data, 'gbk'))
-                    let href = $('#content').children().first().children().eq(5).children().children().first().find('a').attr("href")
-                    if (href !== undefined) {
-                        urls.add(href)
-                        // download(href)
-                    }
-                })
-                .catch((err) => {
-                    console.log(err)
-                })
-                promiseArr.push(promise)
+const fs = Promise.promisifyAll(require('fs'))
+const Url = require('url')
+const Novel = require('./app.js')
+// Novel.download('http://www.wenku8.com/book/2111.htm')
+let host = 'www.wenku8.com'
+const getNovelsUrl = async (url) => {
+    let $ = await Novel.getWithCookie(url)
+    let set = new Set()
+    await Promise.all($('#left .ultop a').map(async (index, item) => {
+        let url = $(item).attr('href')
+        let $$ = await Novel.get(url)
+        $$('#content a').each((index, item) => {
+            let url = $(item).attr('href')
+            if (Url.parse(url).hostname === host) {
+                set.add(url)
             }
-            console.log('1')
-        }
-        //此时promiseArr都是pending状态
-        // console.log(promiseArr)
-        Promise.all(promiseArr).then((data) => {
-            //没有执行
-            resolve(urls)
-        }).catch((err) => {
-            console.log(err)
         })
-    })
-    // console.log(data)
-})
-.then((data) => {
-    console.log(data)
-    data = [...data]
-    fs.writeFile('wenku.json', JSON.stringify(data, null, 4), (err) => {
-        if (err) {
-            console.log(err)
-        }
-    })
-})
-.catch((err) => {
-    console.log(err)
-})
+    }).get())
+    set = Array.from(set)
+    await Promise.map(set, function (url) {
+        return Novel.download(url)
+    }, { concurrency: 10 })
+    // await Promise.all(set.map(async (item, index) => {
+    //     // console.log(item)
+    //     await Novel.download(item)
+    // }))
+}
+getNovelsUrl('http://www.wenku8.com/index.php')
