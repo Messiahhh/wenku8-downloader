dc62c935f120f99ca63e47f6c5d92a48e2b476d2
messiahhh <616348105@qq.com>
2020-05-20 13:55:41 +0800

    改一下介绍

--- app.js
+++ app.js
@@ -6,54 +6,34 @@ const fs = Promise.promisifyAll(require('fs'))
 const Url = require('url')
 const qs = require('querystring')
 
-// 在这里放你的cookie
-// let cookie = ""
-
-let cookie = "Hm_lvt_d72896ddbf8d27c750e3b365ea2fc902=1584112013; jieqiUserInfo=jieqiUserId%3D312317%2CjieqiUserName%3D2497360927%2CjieqiUserGroup%3D3%2CjieqiUserVip%3D0%2CjieqiUserPassword%3D05a671c66aefea124cc08b76ea6d30bb%2CjieqiUserName_un%3D2497360927%2CjieqiUserHonor_un%3D%26%23x65B0%3B%26%23x624B%3B%26%23x4E0A%3B%26%23x8DEF%3B%2CjieqiUserGroupName_un%3D%26%23x666E%3B%26%23x901A%3B%26%23x4F1A%3B%26%23x5458%3B%2CjieqiUserLogin%3D1584118336; jieqiVisitInfo=jieqiUserLogin%3D1584118336%2CjieqiUserId%3D312317; UM_distinctid=170d4cf9e076af-0273d4a6b1d193-366b400c-1fa400-170d4cf9e08874; CNZZDATA1309966=cnzz_eid%3D892072408-1584116280-%26ntime%3D1584116280; CNZZDATA1259916661=493186138-1584117463-%7C1584117463; Hm_lpvt_d72896ddbf8d27c750e3b365ea2fc902=1584118341"
-
-class Scheduler {
-	constructor(count) {
-		this.count = count
-		this.queue = []
-		this.run = []
-	}
-
-	add(task) {
-		this.queue.push(task)
-		return this.schedule()
-	}
-
-	schedule() {
-		if (this.run.length < this.count && this.queue.length) {
-		  	const task = this.queue.shift()
-		  	const promise = task().then(() => {
-		  		this.run.splice(this.run.indexOf(promise), 1)
-		  	})
-		  	this.run.push(promise)
-		  	return promise
-		} else {
-		  	return Promise.race(this.run).then(() => this.schedule())
-		}
-	}
-}
-
+// 调度器，控制发请求的频率
+const Scheduler = require('./scheduler')
 let scheduler = new Scheduler(50)
+
+// 用下面的变量保存你的cookie，面向小白的步骤
+// 1. 登录wenku8.net的网站
+// 2. 按F12打开console
+// 3. 输入document.cookie并回车
+// 4. 将输出的内容复制到下面
+
+let cookie = "Hm_lvt_d72896ddbf8d27c750e3b365ea2fc902=1589952417; UM_distinctid=172308ce92fa5-06cee76426f06f-39624006-1fa400-172308ce93081; jieqiUserInfo=jieqiUserId%3D312317%2CjieqiUserName%3D2497360927%2CjieqiUserGroup%3D3%2CjieqiUserVip%3D0%2CjieqiUserName_un%3D2497360927%2CjieqiUserHonor_un%3D%26%23x65B0%3B%26%23x624B%3B%26%23x4E0A%3B%26%23x8DEF%3B%2CjieqiUserGroupName_un%3D%26%23x666E%3B%26%23x901A%3B%26%23x4F1A%3B%26%23x5458%3B%2CjieqiUserLogin%3D1589952474; jieqiVisitInfo=jieqiUserLogin%3D1589952474%2CjieqiUserId%3D312317; CNZZDATA1309966=cnzz_eid%3D492318477-1589947257-%26ntime%3D1589947257; CNZZDATA1259916661=1822593722-1589947163-%7C1589948180; jieqiVisitId=article_articleviews%3D1715%7C1; Hm_lpvt_d72896ddbf8d27c750e3b365ea2fc902=1589952638"
+
 
 class Novel {
     constructor(obj) {
         Object.assign(this, obj)
     }
 
-    static async getWithCookie(url) {
-        let res = await axios.get(url, {
-            headers: {
-                cookie
-            },
-            responseType: 'arraybuffer'
-        })
-        let $ = cheerio.load(iconv.decode(res.data, 'gbk'))
-        return $
-    }
+    // static async getWithCookie(url) {
+    //     let res = await axios.get(url, {
+    //         headers: {
+    //             cookie
+    //         },
+    //         responseType: 'arraybuffer'
+    //     })
+    //     let $ = cheerio.load(iconv.decode(res.data, 'gbk'))
+    //     return $
+    // }
 
     static async get(url) {
         let res = await axios.get(url, {
@@ -70,7 +50,13 @@ class Novel {
             await fs.mkdirAsync(`./novels/${id}-${name}`)
         }
     }
-
+    /**
+     *
+     *
+     * @static
+     * @param {*} url
+     * @memberof Novel
+     */
     static async download (url) {
 
         let $ = await this.get(url) // 获取某小说的主页
@@ -112,5 +98,6 @@ class Novel {
         }
     }
 }
-// Novel.download('http://www.wenku8.com/book/1715.htm')
-module.exports = Novel
+
+// 你可以把下面的地址换成你想要下载的小说的目录页
+Novel.download('http://www.wenku8.com/book/1.htm')
