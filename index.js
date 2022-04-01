ccafebe86e0178d7a37666b2ea8959b3ac22a18a
huguang@WSL.HG-Desktop <1395943920@qq.com>
2022-04-01 20:10:01 +0800

    impl pic download

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
-const Novel = require('./lib/downloader.js')
+const Novel = require("./lib/downloader.js");
 
-const { program } = require('commander')
+const { program } = require("commander");
 program
-    .version('1.0.0')
-    .description('wenku8 novel downloader')
-    .option('-u, --url <type>', 'website url of the novel you want to download');
-program.parse(process.argv)
+  .version("1.0.0")
+  .description("wenku8 novel downloader")
+  .option("-u, --url <type>", "website url of the novel you want to download");
+program.parse(process.argv);
 
 if (program.url) {
-    Novel.download(program.url)
+  Novel.download(program.url);
 } else {
-    console.log('请使用-u标志输入URL');
+  console.log("请使用-u标志输入URL");
 }
-
-    
